c0707d4dde2f262418c0b26c694cc9b36d9e8642
gianmichelesiano <bandigare@gmail.com>
2019-02-14 09:16:23 +0100

    Change ng

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   stripeKey: 'pk_test_25zrnuBcQ34jtP0XuZLWQqkk',
   //apiUrl: 'http://localhost:5000',
-  apiUrl: 'https://80aafca6.ngrok.io',
+  apiUrl: 'https://18d1a034.ngrok.io',
   firebase: {
     apiKey: "AIzaSyDP_8aNno8CYNmOQRUS740bom-zt2ypZPg",
     authDomain: "jobs-fde7e.firebaseapp.com",
